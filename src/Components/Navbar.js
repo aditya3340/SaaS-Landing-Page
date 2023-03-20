df2f450b803fd818184f01e05a6c1f6cfa575799
aditya3340 <kumar.aditya3340@gmail.com>
2023-03-20 21:20:44 +0530

    href

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,11 +5,11 @@ const Navbar = () => {
   return (
     <div className='navbar'>
         <img src= {vector} alt='navbar vector'/>
-        <a href='#'>HOW IT WORKS</a>
-        <a href='#'>ABOUT</a>
-        <a href='#'>WORK</a>
-        <a href='#'>PRICING</a>
-        <a href='#'>BLOG</a>
+        <a href='/#'>HOW IT WORKS</a>
+        <a href='/#'>ABOUT</a>
+        <a href='/#'>WORK</a>
+        <a href='/#'>PRICING</a>
+        <a href='/#'>BLOG</a>
         <button className='navbar-btn'>GET STARTED</button>
     </div>
   )
