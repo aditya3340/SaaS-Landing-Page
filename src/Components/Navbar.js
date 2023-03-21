0e9b78fa33590a282ed7659aa140ec0223a466f6
aditya3340 <kumar.aditya3340@gmail.com>
2023-03-21 12:21:28 +0530

    test

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,6 +9,11 @@ const Navbar = () => {
     console.log('toggle')
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+
+    document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active")
+    }) )
   }
 
   return (
