8779b6ec67ac4e2ed0a116a2f784ca98dcbbc567
aditya3340 <kumar.aditya3340@gmail.com>
2023-03-21 19:28:49 +0530

    page2 completed

--- src/Components/Page2.js
+++ src/Components/Page2.js
@@ -1,7 +1,10 @@
 import React from "react";
 import stoks from "../Assests/stoks.png";
-import circle from '../Assests/circle.png'
-import square from "../Assests/square.png"
+import circle from "../Assests/circle.png";
+import square from "../Assests/square.png";
+import dudeWithPhone from "../Assests/dudeWithPhone.png";
+import capGirl from "../Assests/capGirl.png";
+import laptopGirl from "../Assests/laptopGirl.png";
 
 const Page2 = () => {
   return (
@@ -25,7 +28,6 @@ const Page2 = () => {
             and deliver incredible results—and we pass those savings on to you
           </p>
         </div>
-        
 
         <div className="card">
           <img src={square} alt="stoks" />
@@ -40,6 +42,56 @@ const Page2 = () => {
           <h4>Fast turnaround times</h4>
           <p>
             TDP was built for flexibility—we quickly adapt and grow as you grow
+          </p>
+        </div>
+      </div>
+
+      <h1 className="head">
+        How <span>Signing Up</span> With The Design Project <span>Works</span>!?
+      </h1>
+      <p className="quote">
+        Take a minute to imagine how you’d feel watching your user count grow,
+        and retention rates stay consistently high. When you prioritize
+        effective UX, better retention and growth are natural consequences.
+        Backed by countless hours of research and testing, TDP blends UX, UI,
+        and Product design into a deliciously smooth process that fuels
+        productivity and success.
+      </p>
+
+      <div className="card-container">
+        <div className="card">
+          <div className="circle">01</div>
+          <p className="signup_tut">Sign up for one of our services</p>
+          <img
+            src={dudeWithPhone}
+            alt="phone boy"
+            
+          />
+          <p>
+            Sign up for one of our affordable subscription play which offer a
+            variety of options to fit your unique needs.
+          </p>
+        </div>
+        <div className="card">
+          <div className="circle">02</div>
+          <p className="signup_tut">Meet your perfect match</p>
+          <img
+            src={laptopGirl}
+            alt="phone boy"
+            
+          />
+          <p>
+            We match you with the best TDP design expert for your needs. Then we
+            schedule a call to learn about your startup.
+          </p>
+        </div>
+        <div className="card">
+          <div className="circle">03</div>
+          <p className="signup_tut">The first sprint begins (hold on tight)</p>
+          <img src={capGirl} alt="phone boy" />
+          <p>
+            TDP manages the project, learns the intimate details of what your
+            users want, and delivers impactful design iterations.
           </p>
         </div>
       </div>
